4f60fe92300a4d6676bf2b8642f6e5f741e6ba74
ST10263301 <ST10263301@vcconnect.edu.za>
2025-05-14 14:16:23 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AgriEnergyConnectApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
